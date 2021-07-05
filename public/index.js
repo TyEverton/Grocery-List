1716e090ea72a419ad81130a3356dd24a89c552f
Ty Everton <everton_915@yahoo.com>
2021-07-05 15:59:45 -0600

    first

---
+++ public/index.js
@@ -0,0 +1,34 @@
+document.querySelector("form").addEventListener("submit", addItem)
+const message = document.querySelector("#message")
+
+
+
+function addItem(event) {
+  event.preventDefault()
+  let inputField = document.querySelector("input")
+
+  const item = document.createElement("li")
+
+  const itemName = document.createElement("span")
+  itemName.textContent = inputField.value
+  item.appendChild(itemName)
+
+  const deleteButton = document.createElement("button")
+  deleteButton.textContent = ("REMOVE")
+  deleteButton.addEventListener("click", deleteItem)
+  item.appendChild(deleteButton)
+
+  const list = document.querySelector("ul")
+  list.appendChild(item)
+
+  inputField.value = ''
+}
+
+
+function deleteItem(event) {
+
+  event.target.parentNode.remove()
+}
+
+
+
