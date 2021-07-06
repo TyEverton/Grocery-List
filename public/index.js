a1a681172cd188d2079afdfcc5a453dc5dcf9535
Ty Everton <everton_915@yahoo.com>
2021-07-06 11:45:45 -0600

    final

--- public/index.js
+++ public/index.js
@@ -5,33 +5,26 @@ function addItem(event) {
   event.preventDefault()
   let inputField = document.querySelector("input")
 
-  const item = document.createElement("li")
+  const item = document.createElement("li") //creates the list of users items
 
   const itemName = document.createElement("span")
   itemName.textContent = inputField.value
-  item.appendChild(itemName) //adds the item to the already existing element (UL)
+  item.appendChild(itemName) //allows users to name items and add them to the list
 
   const deleteButton = document.createElement("button")
   deleteButton.textContent = ("REMOVE")
   deleteButton.addEventListener("click", deleteItem)
-  item.appendChild(deleteButton) //deletes the item from the existing element (UL)
-  
+  item.appendChild(deleteButton) //creates remove button | deletes the item from the existing element (UL)
 
   const list = document.querySelector("ul")
-  list.appendChild(item) 
+  list.appendChild(item)  //adds items to the list once the add button is used
 
   inputField.value = ''
-}
 
+}
 
 function deleteItem(event) {
   event.preventDefault()
   event.target.parentNode.remove()
-}
-
-function crossOffItem(event) {
-  event.preventDefault()
-  event.target.parentNode.strike()
-  item.addEventListener("click", crossOffItem)
-}
+} //deletes each item once the remove button is used
 
